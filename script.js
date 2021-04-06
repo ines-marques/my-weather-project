4ab9fd879f30c22b321cbd67274dd7b1c4089caa
ines-marques <81305906+ines-marques@users.noreply.github.com>
2021-04-06 15:31:52 +0100

    update minutes

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
       let currentTime = document.querySelector("#current-time");
       let hours = now.getHours();
       let minutes = now.getMinutes();
+      if (minutes < 10) {
+      minutes = `0${minutes}`;
+      }
       let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
       let day = days[now.getDay()];
 
