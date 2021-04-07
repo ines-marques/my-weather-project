96eb4c07011b557a5ad9b973a4881a260f4878c3
ines-marques <81305906+ines-marques@users.noreply.github.com>
2021-04-07 08:41:46 +0100

    updated hours

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
       let now = new Date();
       let currentTime = document.querySelector("#current-time");
       let hours = now.getHours();
+      if (hours < 10) {
+      hours = `0${hours}`;
+      }
       let minutes = now.getMinutes();
       if (minutes < 10) {
       minutes = `0${minutes}`;
